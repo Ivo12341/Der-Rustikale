d6a1df250695b636dbe493e871530a0aeb8dc607
Ivo Graber <ivo.graber@ict-campus.net>
2024-10-25 12:12:00 +0200

    Added create method for GUI

--- src/gui_view.rs
+++ src/gui_view.rs
@@ -3,10 +3,15 @@ use egui::{ Context };
 use crate::db_repo::DbRepo;
 use crate::gui_view::Tab::{CREATE, DELETE, HOME, SEARCH, STATUS, VIEW};
 use crate::repository::Repository;
+use crate::task::{Status, Task};
 
 pub struct ToDoApp {
     repo: Box<dyn Repository>,
-    tab: Tab
+    tab: Tab,
+    title_input: String,
+    due_date_input: String,
+    error: [String; 2],
+    prio: i32
 }
 
 enum Tab {
@@ -20,7 +25,13 @@ enum Tab {
 
 impl ToDoApp {
     pub fn new(repo2: Box<dyn Repository>) -> Self {
-        Self { repo: repo2, tab: HOME }
+        Self { repo: repo2,
+            tab: ToDoApp::default().tab,
+            title_input: ToDoApp::default().title_input,
+            due_date_input: ToDoApp::default().due_date_input,
+            error: ToDoApp::default().error,
+            prio: ToDoApp::default().prio
+        }
     }
 }
 
@@ -28,7 +39,11 @@ impl Default for ToDoApp {
     fn default() -> Self {
         Self {
             repo: Box::new(DbRepo::new("todo.db")),
-            tab: HOME
+            tab: HOME,
+            title_input: String::from(""),
+            due_date_input: String::from(""),
+            error: [String::from(""), String::from("")],
+            prio: 0
         }
     }
 }
@@ -58,6 +73,34 @@ impl eframe::App for ToDoApp {
                     ui.heading("Under Construction");
                     if ui.button("back").clicked() {
                         self.tab = HOME
+                    }
+                    let lulu = ui.label("Title");
+                    ui.text_edit_singleline(&mut self.title_input).labelled_by(lulu.id);
+                    let lulu2 = ui.label("Due Date (yyyy-mm-dd)");
+                    ui.text_edit_singleline(&mut self.due_date_input).labelled_by(lulu2.id);
+                    ui.colored_label(egui::Color32::RED, &self.error[0]);
+                    if !Task::match_date_format(&self.due_date_input) {
+                        self.error[0] = String::from("Nich jültüsch");
+                    }
+                    else {
+                        self.error = ToDoApp::default().error
+                    }
+                    ui.add(egui::Slider::new(&mut self.prio, 0..=100).text("Priorität"));
+                    ui.colored_label(egui::Color32::RED, &self.error[1]);
+                    if ui.button("Speichern").clicked() {
+                        let task = Task {
+                            title: self.title_input.to_string(),
+                            due_date: self.due_date_input.to_string(),
+                            priority: self.prio,
+                            status: Status::NotStarted,
+                        };
+                        if self.repo.save_task(&task) {
+                            self.error[1] = String::from("");
+                            self.tab = HOME
+                        }
+                        else {
+                            self.error[1] = String::from("Task already exists");
+                        }
                     }
                 }
                 VIEW => {
